a68b4684280d17b1923762e948971231d2d48fbe
Zane Rockenbaugh <zane@liquid-labs.com>
2023-06-24 15:42:11 -0500

    moved key location funcs here and tested

---
+++ src/test/locations.test.js
@@ -0,0 +1,36 @@
+/* global describe expect test */
+import * as fsPath from 'node:path'
+
+import * as locations from '../locations'
+
+describe('LIQ_HOME', () => {
+  test("defaults to '.liq' in users home dir", () =>
+    expect(locations.LIQ_HOME()).toBe(fsPath.join(process.env.HOME, '.liq')))
+
+  test('can be overridden by setting environment var LIQ_HOME', () => {
+    const newHome = fsPath.sep + 'foo'
+    process.env.LIQ_HOME = newHome
+    try {
+      expect(locations.LIQ_HOME()).toBe(newHome)
+    }
+    finally {
+      delete process.env.LIQ_HOME
+    }
+  })
+})
+
+describe('LIQ_PLAYGROUND', () => {
+  test("defaults to '<LIQ_HOME>/playground' in users home dir", () =>
+    expect(locations.LIQ_PLAYGROUND()).toBe(fsPath.join(locations.LIQ_HOME(), 'playground')))
+
+  test('can be overridden by setting environment var LIQ_PLAYGROUND', () => {
+    const newPlayground = fsPath.sep + 'foo'
+    process.env.LIQ_PLAYGROUND = newPlayground
+    try {
+      expect(locations.LIQ_PLAYGROUND()).toBe(newPlayground)
+    }
+    finally {
+      delete process.env.LIQ_PLAYGROUND
+    }
+  })
+})
