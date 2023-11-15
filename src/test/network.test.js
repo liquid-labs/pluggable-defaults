84f2046c2c7c7aec7a2ec1ec190d28f2c932b5ba
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-15 08:39:55 -0600

    update var names from LIQ to PLUGABLE

--- src/test/network.test.js
+++ src/test/network.test.js
@@ -2,13 +2,13 @@
 import * as network from '../network'
 
 describe('LIQ_PORT', () => {
-  test('defaults to 32600', () => expect(network.LIQ_PORT()).toBe(32600))
+  test('defaults to 32600', () => expect(network.PLUGABLE_PORT()).toBe(32600))
 
   test('can be overridden by setting environment var LIQ_PORT', () => {
     const newPort = 32651
     process.env.LIQ_PORT = newPort
     try {
-      expect(network.LIQ_PORT()).toBe(newPort + '')
+      expect(network.PLUGABLE_PORT()).toBe(newPort)
     }
     finally {
       delete process.env.LIQ_PORT
