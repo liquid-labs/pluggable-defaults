cad0882ffa191307db007da67aa9818f09c234c6
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-15 11:27:10 -0600

    Remove server specific defaults and add PLUGABLE_CLI_SETTINGS_PATH

--- src/locations.mjs
+++ src/locations.mjs
@@ -1,6 +1,9 @@
 import fsPath from 'node:path'
 
-const configDir = process.env.XDG_CONFIG_HOME || fsPath.join(process.env.HOME, '.config')
+import { configDir } from './config-dir'
+
+export const PLUGABLE_CLI_SETTINGS_PATH = () => process.env.PLUGABLE_CLI_SETTINGS_PATH
+  || fsPath.join(configDir, 'plugable', 'cli-settings.yaml')
 
 export const PLUGABLE_PLAYGROUND = () => process.env.PLUGABLE_PLAYGROUND
   || process.env.LIQ_PLAYGROUND
--- src/test/locations.test.js
+++ src/test/locations.test.js
@@ -1,10 +1,27 @@
 /* global describe expect test */
 import * as fsPath from 'node:path'
 
+import { configDir } from '../config-dir'
 import * as locations from '../locations'
 
+describe('PLUGABLE_CLI_SETTINGS_PATH', () => {
+  test("defaults to '<config-dir>/plugable/cli-settings.yaml'", () => 
+    expect(locations.PLUGABLE_CLI_SETTINGS_PATH()).toBe(fsPath.join(configDir, 'plugable', 'cli-settings.yaml')))
+
+  test('can be overriden by setting environment var PLUGABLE_CLI_SETTINGS_PATH', () => {
+    const newCLISettingsPath = fsPath.sep + 'foo'
+    process.env.PLUGABLE_CLI_SETTINGS_PATH = newCLISettingsPath
+    try {
+      expect(locations.PLUGABLE_CLI_SETTINGS_PATH()).toBe(newCLISettingsPath)
+    }
+    finally {
+      delete process.env.PLUGABLE_CLI_SETTINGS_PATH
+    }
+  })
+})
+
 describe('PLUGABLE_PLAYGROUND', () => {
-  test("defaults to '${HOME}/playground' in users home dir", () =>
+  test("defaults to '${HOME}/playground' in users home dir", () => // eslint-disable-line no-template-curly-in-string
     expect(locations.PLUGABLE_PLAYGROUND()).toBe(fsPath.join(process.env.HOME, 'playground')))
 
   test.each(['LIQ_PLAYGROUND', 'PLUGABLE_PLAYGROUND'])('can be overridden by setting environment var %s', (envVar) => {
